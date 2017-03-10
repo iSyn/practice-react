5cd011d670f7d80c6d2dadd997b52ab79294cbf5
Synclair Wang <synclairwang11@yahoo.com>
2017-03-10 01:13:43 -0500

    practicing

---
+++ public/app.jsx
@@ -0,0 +1,51 @@
+var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'Default',
+      message: 'This is the default message'
+    }
+  },
+
+  getInitialState: function() {
+    return {
+      name: this.props.name
+    }
+  },
+
+  onButtonClick: function(e) {
+    e.preventDefault();
+    var name = this.refs.inputName.value;
+
+    this.setState({
+      name: name
+    })
+  },
+
+  render: function() {
+    var name = this.state.name;
+    var message = this.props.message;
+
+    return(
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref='inputName'/>
+          <button>Set New Name</button>
+        </form>
+
+      </div>
+    );
+  }
+});
+
+var firstName = 'Synclair'
+
+ReactDOM.render(
+  <Greeter
+    name={firstName}
+    message='Message from props'
+    />,
+  document.getElementById('app')
+);
