ad0b6ecb99bb8d56dc121433a4a1212e53e1eb39
Synclair Wang <synclairwang11@yahoo.com>
2017-03-10 01:21:56 -0500

    practicing

--- public/app.jsx
+++ public/app.jsx
@@ -14,11 +14,17 @@ var Greeter = React.createClass({
 
   onButtonClick: function(e) {
     e.preventDefault();
-    var name = this.refs.inputName.value;
 
-    this.setState({
-      name: name
-    })
+    var nameRef = this.refs.inputName
+
+    var name = nameRef.value;
+    nameRef.value = '';
+
+    if (typeof name === 'string' && name.length > 0) {
+      this.setState({
+        name: name
+      })
+    }
   },
 
   render: function() {
