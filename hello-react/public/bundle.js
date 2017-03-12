0f3217f394e128094bbb8efa5a91517db6164b45
Synclair Wang <synclairwang11@yahoo.com>
2017-03-11 23:03:42 -0500

    coding some more

--- hello-react/public/bundle.js
+++ hello-react/public/bundle.js
@@ -49,114 +49,7 @@
 	var React = __webpack_require__(1);
 	var ReactDOM = __webpack_require__(158);
 
-	var GreeterMessage = React.createClass({
-	  displayName: 'GreeterMessage',
-
-	  render: function render() {
-	    var name = this.props.name;
-	    var message = this.props.message;
-	    return React.createElement(
-	      'div',
-	      null,
-	      React.createElement(
-	        'h1',
-	        null,
-	        'Hello ',
-	        name,
-	        '!'
-	      ),
-	      React.createElement(
-	        'p',
-	        null,
-	        message
-	      )
-	    );
-	  }
-	});
-
-	var GreeterForm = React.createClass({
-	  displayName: 'GreeterForm',
-
-	  onFormSubmit: function onFormSubmit(e) {
-	    e.preventDefault();
-
-	    var updates = {};
-	    var name = this.refs.inputName.value;
-	    var message = this.refs.inputMessage.value;
-
-	    if (name.length > 0) {
-	      this.refs.inputName.value = '';
-	      updates.name = name;
-	    }
-
-	    if (message.length > 0) {
-	      this.refs.inputMessage.value = '';
-	      updates.message = message;
-	    }
-
-	    this.props.onNewData(updates);
-	  },
-
-	  render: function render() {
-	    return React.createElement(
-	      'form',
-	      { onSubmit: this.onFormSubmit },
-	      React.createElement(
-	        'div',
-	        null,
-	        React.createElement('input', { type: 'text', ref: 'inputName', placeholder: 'Enter name' })
-	      ),
-	      React.createElement(
-	        'div',
-	        null,
-	        React.createElement('textarea', { ref: 'inputMessage', placeholder: 'Enter message' })
-	      ),
-	      React.createElement(
-	        'div',
-	        null,
-	        React.createElement(
-	          'button',
-	          null,
-	          'Submit'
-	        )
-	      )
-	    );
-	  }
-	});
-
-	var Greeter = React.createClass({
-	  displayName: 'Greeter',
-
-	  getDefaultProps: function getDefaultProps() {
-	    return {
-	      name: 'Default',
-	      message: 'This is the default message'
-	    };
-	  },
-
-	  getInitialState: function getInitialState() {
-	    return {
-	      name: this.props.name,
-	      messsage: this.props.message
-	    };
-	  },
-
-	  handleNewData: function handleNewData(updates) {
-	    this.setState(updates);
-	  },
-
-	  render: function render() {
-	    var name = this.state.name;
-	    var message = this.state.message;
-
-	    return React.createElement(
-	      'div',
-	      null,
-	      React.createElement(GreeterMessage, { name: name, message: message }),
-	      React.createElement(GreeterForm, { onNewData: this.handleNewData })
-	    );
-	  }
-	});
+	var Greeter = __webpack_require__(159);
 
 	var firstName = 'Synclair';
 
@@ -19853,6 +19746,148 @@
 
 	module.exports = __webpack_require__(3);
 
+
+/***/ },
+/* 159 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var React = __webpack_require__(1);
+
+	var GreeterMessage = __webpack_require__(160);
+	var GreeterForm = __webpack_require__(161);
+
+	var Greeter = React.createClass({
+	  displayName: 'Greeter',
+
+	  getDefaultProps: function getDefaultProps() {
+	    return {
+	      name: 'Default',
+	      message: 'This is the default message'
+	    };
+	  },
+
+	  getInitialState: function getInitialState() {
+	    return {
+	      name: this.props.name,
+	      messsage: this.props.message
+	    };
+	  },
+
+	  handleNewData: function handleNewData(updates) {
+	    this.setState(updates);
+	  },
+
+	  render: function render() {
+	    var name = this.state.name;
+	    var message = this.state.message;
+
+	    return React.createElement(
+	      'div',
+	      null,
+	      React.createElement(GreeterMessage, { name: name, message: message }),
+	      React.createElement(GreeterForm, { onNewData: this.handleNewData })
+	    );
+	  }
+	});
+
+	module.exports = Greeter;
+
+/***/ },
+/* 160 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var React = __webpack_require__(1);
+
+	var GreeterMessage = React.createClass({
+	  displayName: 'GreeterMessage',
+
+	  render: function render() {
+	    var name = this.props.name;
+	    var message = this.props.message;
+	    return React.createElement(
+	      'div',
+	      null,
+	      React.createElement(
+	        'h1',
+	        null,
+	        'Hello ',
+	        name,
+	        '!'
+	      ),
+	      React.createElement(
+	        'p',
+	        null,
+	        message
+	      )
+	    );
+	  }
+	});
+
+	module.exports = GreeterMessage;
+
+/***/ },
+/* 161 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var React = __webpack_require__(1);
+
+	var GreeterForm = React.createClass({
+	  displayName: 'GreeterForm',
+
+	  onFormSubmit: function onFormSubmit(e) {
+	    e.preventDefault();
+
+	    var updates = {};
+	    var name = this.refs.inputName.value;
+	    var message = this.refs.inputMessage.value;
+
+	    if (name.length > 0) {
+	      this.refs.inputName.value = '';
+	      updates.name = name;
+	    }
+
+	    if (message.length > 0) {
+	      this.refs.inputMessage.value = '';
+	      updates.message = message;
+	    }
+
+	    this.props.onNewData(updates);
+	  },
+
+	  render: function render() {
+	    return React.createElement(
+	      'form',
+	      { onSubmit: this.onFormSubmit },
+	      React.createElement(
+	        'div',
+	        null,
+	        React.createElement('input', { type: 'text', ref: 'inputName', placeholder: 'Enter name' })
+	      ),
+	      React.createElement(
+	        'div',
+	        null,
+	        React.createElement('textarea', { ref: 'inputMessage', placeholder: 'Enter message' })
+	      ),
+	      React.createElement(
+	        'div',
+	        null,
+	        React.createElement(
+	          'button',
+	          null,
+	          'Submit'
+	        )
+	      )
+	    );
+	  }
+	});
+
+	module.exports = GreeterForm;
 
 /***/ }
 /******/ ]);
---
+++ react-boilerplate/public/app.jsx
@@ -0,0 +1,15 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+var Greeter = require('Greeter')
+
+
+
+var firstName = 'Synclair'
+
+ReactDOM.render(
+  <Greeter
+    name={firstName}
+    />,
+  document.getElementById('app')
+);
