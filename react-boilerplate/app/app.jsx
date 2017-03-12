524c8ab1be496017dc30e8b1b6bce07812ce6594
Synclair Wang <synclairwang11@yahoo.com>
2017-03-11 23:11:08 -0500

    bleh

--- react-boilerplate/app/app.jsx
+++ react-boilerplate/app/app.jsx
@@ -1,15 +1,7 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 
-var Greeter = require('Greeter')
-
-
-
-var firstName = 'Synclair'
-
 ReactDOM.render(
-  <Greeter
-    name={firstName}
-    />,
+  <h1>Boilerplate App!</h1>,
   document.getElementById('app')
 );
