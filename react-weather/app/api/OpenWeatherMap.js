315d291c9cffdcaf77962a00026fb324b8d35c29
Synclair Wang <synclairwang11@yahoo.com>
2017-03-13 03:28:03 -0400

    working api calls and everything

--- react-weather/app/api/OpenWeatherMap.js
+++ react-weather/app/api/OpenWeatherMap.js
@@ -1,9 +1,22 @@
 var axios = require('axios')
 
-const OPEN_WEATHER_MAP_URL = 'http://openweathermap.org/data/2.5/weather?&appid=b3bcc1d3ffa7a2e5ef5664be36886486&units=imperial';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather';
+const API_KEY = '726349cbda86fa3f4ecfbf48282d90de'
+var units = 'imperial'
 
 module.exports = {
   getTemp: function(location) {
+    var encodedLocation = encodeURIComponent(location)
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}?appid=${API_KEY}&units=${units}&q=${encodedLocation}`
 
+    return axios.get(requestUrl).then(function(res) {
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message)
+      } else {
+        return res.data.main.temp
+      }
+    }, function(res) {
+      throw new Error(res.data.message)
+    })
   }
 }
--- react-weather/app/components/Weather.jsx
+++ react-weather/app/components/Weather.jsx
@@ -2,30 +2,55 @@ var React = require('react')
 
 var WeatherForm = require('WeatherForm')
 var WeatherMessage = require('WeatherMessage')
+var openWeatherMap = require('openWeatherMap')
 
 var Weather = React.createClass({
 
   getInitialState: function() {
     return{
-      location: 'Miami',
-      temp: 88
+      isLoading: false
     }
   },
 
   handleSearch: function(location) {
-    // this.setState({
-    //   location: location,
-    //   temp: 23
-    // })
+    var that = this
+
+    this.setState({
+      isLoading: true
+    })
+
+    openWeatherMap.getTemp(location).then(function(temp) {
+      // 'this' is in a function so we can use 'this'
+      // So you create a variable up top binding 'this' to 'that'
+      that.setState({
+        isLoading: false,
+        location: location,
+        temp: temp
+      })
+    }, function(err) {
+      alert(err)
+      that.setState({
+        isLoading: false
+      })
+    })
   },
 
   render: function() {
-    var {temp, location} = this.state
+    var {isLoading, temp, location} = this.state
+
+    function renderMessage() {
+      if (isLoading) {
+        return <h3>Fetching the weather...</h3>
+      } else if (temp && location) {
+        return <WeatherMessage location={location} temp={temp}/>
+      }
+    }
+
     return(
       <div>
         <h3>Get Weather</h3>
         <WeatherForm onSearch={this.handleSearch}/>
-        <WeatherMessage location={location} temp={temp}/>
+        {renderMessage()}
       </div>
     )
   }
