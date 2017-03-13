df8e2facc8708fd3732fe37c0bf3b5f2a557cb6f
Synclair Wang <synclairwang11@yahoo.com>
2017-03-13 00:56:52 -0400

    added links to react router

--- react-weather/app/app.jsx
+++ react-weather/app/app.jsx
@@ -3,10 +3,15 @@ var ReactDOM = require('react-dom')
 var {Route, Router, IndexRoute, hashHistory} = require('react-router')
 
 var Main = require('Main')
+var Weather = require('Weather')
+var About = require('About')
 
 ReactDOM.render(
   <Router history={hashHistory}>
-    <Route path='/' component={Main}></Route>
+    <Route path='/' component={Main}>
+      <IndexRoute component={Weather}/>
+      <Route path='about' component={About} />
+    </Route>
   </Router>,
   document.getElementById('app')
 );
