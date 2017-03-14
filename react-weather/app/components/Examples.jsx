66994807671e1470a0486cebe0fde3011cfd4655
Synclair Wang <synclairwang11@yahoo.com>
2017-03-14 02:49:43 -0400

    using more foundation styling

--- react-weather/app/components/Examples.jsx
+++ react-weather/app/components/Examples.jsx
@@ -1,18 +1,19 @@
 var React = require('react')
-
-// var Examples = React.createClass({
-//   render: function() {
-//     return(
-//       <h3>Example component</h3>
-//     )
-//   }
-// })
+var {Link} = require('react-router')
 
 var Examples = (props) => {
   return(
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to the examples page!</p>
+      <h1 className='text-center'>Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=New+York'>New York, NY</Link>
+        </li>
+        <li>
+          <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+        </li>
+      </ol>
     </div>
   )
 }
