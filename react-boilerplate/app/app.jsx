5da79531bdbb71424798f0de8f1d0119d22c625f
Synclair Wang <synclairwang11@yahoo.com>
2017-03-20 01:50:26 -0400

    added updated boilerplate

--- react-boilerplate/app/app.jsx
+++ react-boilerplate/app/app.jsx
@@ -2,10 +2,6 @@ var React = require('react')
 var ReactDOM = require('react-dom')
 var {Route, Router, IndexRoute, hashHistory} = require('react-router')
 
-var Main = require('Main')
-var Timer = require('Timer')
-var Countdown = require('Countdown')
-
 // Load foundation
 $(document).foundation()
 
@@ -13,11 +9,6 @@ $(document).foundation()
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path='/' component={Main}>
-      <IndexRoute component={Timer}/>
-      <Route path='countdown' component={Countdown}/>
-    </Route>
-  </Router>,
+  <p>React Boilerplate</p>,
   document.getElementById('app')
 );
