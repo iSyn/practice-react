0563891968784ec81f2342f05bdb9bda2348b370
Synclair Wang <synclairwang11@yahoo.com>
2017-03-21 01:36:55 -0400

    added search functionality

--- react-todo/app/api/TodoAPI.js
+++ react-todo/app/api/TodoAPI.js
@@ -19,6 +19,44 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos : []
+  },
 
+  filterTodos: function(todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    // Filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    })
+
+    // Filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      // var lowercased = searchText.toLowerCase()
+      var text = todo.text.toLowerCase()
+      // return todo.indexOf(lowercased)
+      return searchText.length === 0 || text.indexOf(searchText) > -1
+    })
+
+    // Sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        return -1
+      } else if (a.completed && !b.completed) {
+        return 1
+      } else {
+        return 0
+      }
+    })
+
+
+
+    return filteredTodos
   }
+
+
+
+
+
+
+
 }
