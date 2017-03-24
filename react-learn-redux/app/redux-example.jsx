ba620a3b81ff477f3fbe48fb48662e3afeb5ae4b
Synclair Wang <synclairwang11@yahoo.com>
2017-03-23 23:53:44 -0400

    added actioons for adding and removing hobbies and movies

--- react-learn-redux/app/redux-example.jsx
+++ react-learn-redux/app/redux-example.jsx
@@ -2,7 +2,15 @@ var redux = require('redux')
 
 console.log('starting redux example')
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+}
+
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var reducer = (state = stateDefault, action) => {
   // state = state || {name: 'Anonymous'}
 
   switch (action.type) {
@@ -11,10 +19,44 @@ var reducer = (state = {name: 'Anonymous'}, action) => {
         ...state,
         name: action.name
       }
+    case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyId++,
+            hobbies: action.hobby
+          }
+        ]
+      }
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+      }
+    case 'ADD_MOVIE':
+      return {
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            id: nextMovieId++,
+            title: action.title,
+            genre: action.genre
+          }
+        ]
+      }
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter((movie) => movie.id !== action.id)
+      }
     default:
       return state
   }
 }
+
 var store = redux.createStore(reducer, redux.compose(
   window.devToolsExtension ? window.devToolsExtension() : f => f
 ))
@@ -24,6 +66,7 @@ var unsubscribe = store.subscribe(() => {
   var state = store.getState()
   console.log('Name is', state.name)
   document.getElementById('app').innerHTML = state.name
+  console.log('New State', store.getState())
 })
 
 // unsubscribe()
@@ -37,6 +80,39 @@ store.dispatch({
 })
 
 store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
+})
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Walking'
+})
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+})
+
+store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Emily'
 })
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Mad Max',
+  genre: 'Action'
+})
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Shrek',
+  genre: 'Animation'
+})
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 1
+})
+
