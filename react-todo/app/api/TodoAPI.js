43c5b54c4b2d05042f6297bb7b00d9044f3b806d
Synclair Wang <synclairwang11@yahoo.com>
2017-03-25 01:44:37 -0400

    react redux woo

--- react-todo/app/api/TodoAPI.js
+++ react-todo/app/api/TodoAPI.js
@@ -1,62 +1,49 @@
-var $ = require('jquery')
+var $ = require('jquery');
 
 module.exports = {
-  setTodos: function(todos) {
+  setTodos: function (todos) {
     if ($.isArray(todos)) {
-      localStorage.setItem('todos', JSON.stringify(todos))
-      return todos
+      localStorage.setItem('todos', JSON.stringify(todos));
+      return todos;
     }
   },
-
-  getTodos: function() {
-    var stringTodos = localStorage.getItem('todos')
-    var todos = []
+  getTodos: function () {
+    var stringTodos = localStorage.getItem('todos');
+    var todos = [];
 
     try {
-      todos = JSON.parse(stringTodos)
-    } catch(err) {
-      console.log(err)
+      todos = JSON.parse(stringTodos);
+    } catch (e) {
+
     }
 
-    return $.isArray(todos) ? todos : []
+    return $.isArray(todos) ? todos : [];
   },
-
-  filterTodos: function(todos, showCompleted, searchText) {
+  filterTodos: function (todos, showCompleted, searchText) {
     var filteredTodos = todos;
 
     // Filter by showCompleted
     filteredTodos = filteredTodos.filter((todo) => {
       return !todo.completed || showCompleted;
-    })
+    });
 
     // Filter by searchText
     filteredTodos = filteredTodos.filter((todo) => {
-      // var lowercased = searchText.toLowerCase()
-      var text = todo.text.toLowerCase()
-      // return todo.indexOf(lowercased)
-      return searchText.length === 0 || text.indexOf(searchText) > -1
-    })
+      var text = todo.text.toLowerCase();
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
 
     // Sort todos with non-completed first
     filteredTodos.sort((a, b) => {
       if (!a.completed && b.completed) {
-        return -1
+        return -1;
       } else if (a.completed && !b.completed) {
-        return 1
+        return 1;
       } else {
-        return 0
+        return 0;
       }
-    })
+    });
 
-
-
-    return filteredTodos
+    return filteredTodos;
   }
-
-
-
-
-
-
-
-}
+};
--- react-todo/app/components/AddTodo.jsx
+++ react-todo/app/components/AddTodo.jsx
@@ -1,32 +1,30 @@
-var React = require('react')
-var {connect} = require('react-redux')
-var actions = require('actions')
+var React = require('react');
+var {connect} = require('react-redux');
+var actions = require('actions');
 
 export var AddTodo = React.createClass({
-
-  handleSubmit: function(e) {
-    e.preventDefault()
-    var {dispatch} = this.props
+  handleSubmit: function (e) {
+    e.preventDefault();
+    var {dispatch} = this.props;
     var todoText = this.refs.todoText.value;
 
     if (todoText.length > 0) {
-      this.refs.todoText.value = ''
-      dispatch(actions.addTodo(todoText))
+      this.refs.todoText.value = '';
+      dispatch(actions.addTodo(todoText));
     } else {
-      this.refs.todoText.focus()
+      this.refs.todoText.focus();
     }
   },
-
-  render: function() {
-    return(
-      <div className='container__footer'>
+  render: function () {
+    return (
+      <div className="container__footer">
         <form onSubmit={this.handleSubmit}>
-          <input type="text" placeholder='What do you need to do?' ref='todoText'/>
-          <button className='button expanded'>Add Todo</button>
+          <input type="text" ref="todoText" placeholder="What do you need to do?"/>
+          <button className="button expanded">Add Todo</button>
         </form>
       </div>
-    )
+    );
   }
-})
+});
 
-export default connect()(AddTodo)
+export default connect()(AddTodo);
